fac1d7b5b4983fdd9b1d059bee40b1a719e9ee9b
MBoretto <marco.bore@gmail.com>
2016-09-25 07:12:12 +0200

    init bot config file

---
+++ config/telegram.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | nosticker
+    |--------------------------------------------------------------------------
+    |
+    | Token for nostricker
+    |
+    */
+    'nosticker' => ['name' => 'nostickerbot', 'token' => 'your-toker', 'botan_token' => 'your-botan-token'],
+];
